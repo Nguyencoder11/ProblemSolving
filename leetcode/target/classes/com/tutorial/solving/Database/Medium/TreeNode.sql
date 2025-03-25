2188313cc6b56685c515ae2c9305fcd150b0d746
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2025-03-25 17:30:04 +0700

    added in branch main

--- leetcode/target/classes/com/tutorial/solving/Database/Medium/TreeNode.sql
+++
@@ -1,7 +0,0 @@
-select id, 
-    case 
-        when p_id is null then 'Root'
-        when id not in (select p_id from Tree where p_id is not null) then 'Leaf'
-        else 'Inner'
-    end as type
-from Tree;
